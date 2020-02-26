ade84691fe8606ba6934673efd286d52525ffcc1
Sanhe Hu <husanhe@gmail.com>
2020-02-26 17:14:22 -0500

    raise error in int test

--- tests/e99_serverless_project_integration_test.py
+++ tests/e99_serverless_project_integration_test.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import sys, traceback
 import boto3
 from datetime import datetime, timezone
 from a_micro_service.devops.config_init import config
 
-boto_ses = boto3.session.Session(
-    profile_name=config.AWS_PROFILE_FOR_BOTO3.get_value(),
-    region_name=config.AWS_REGION.get_value(),
-)
-iam = boto_ses.client("iam")
+try:
+    boto_ses = boto3.session.Session(
+        profile_name=config.AWS_PROFILE_FOR_BOTO3.get_value(),
+        region_name=config.AWS_REGION.get_value(),
+    )
+    iam = boto_ses.client("iam")
 
-res_get_role = iam.get_role(RoleName="pygitrepo-{}-ec2-role".format(config.ENVIRONMENT_NAME.get_value()))
-create_at = res_get_role["Role"]["CreateDate"]
-now = datetime.utcnow()
-now = now.replace(tzinfo=timezone.utc)
-assert (now - create_at).total_seconds() <= 120 # 2 minutes
-assert 1 == 0
+    res_get_role = iam.get_role(RoleName="pygitrepo-{}-ec2-role".format(config.ENVIRONMENT_NAME.get_value()))
+    create_at = res_get_role["Role"]["CreateDate"]
+    now = datetime.utcnow()
+    now = now.replace(tzinfo=timezone.utc)
 
-cft = boto_ses.client("cloudformation")
-cft.delete_stack(StackName="pygitrepo={}".format(config.ENVIRONMENT_NAME.get_value()))
+    assert (now - create_at).total_seconds() <= 120 # 2 minutes
+
+    cft = boto_ses.client("cloudformation")
+    cft.delete_stack(StackName="pygitrepo-{}".format(config.ENVIRONMENT_NAME.get_value()))
+
+    assert 1 == 0
+except:
+    traceback.print_exc()
+    sys.exit(1)
